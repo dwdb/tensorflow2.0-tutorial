686d098533a2e5401d8e309874ac51a563d179ad
mlc1993 <459374521@qq.com>
2020-05-06 01:54:47 +0800

    estimator实现dnn用于手写数字识别

--- estimator_dnn_mnist.py
+++ estimator_dnn_mnist.py
@@ -1,39 +1,48 @@
-import os
-
 import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.python.platform import tf_logging
 
-from tensorflow.examples.tutorials.mnist import input_data
+tf_logging.set_verbosity(tf_logging.INFO)
 
-tf.logging.set_verbosity(tf.logging.INFO)
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+x_train = x_train.reshape(-1, 784).astype(np.float32) / 255.
+x_test = x_test.reshape(-1, 784).astype(np.float32) / 255.
+y_train = y_train.astype(np.int32)
+y_test = y_test.astype(np.int32)
 
-mnist_path = os.path.expanduser('~') + '/.keras/datasets/minist'
-mnist = input_data.read_data_sets(mnist_path, one_hot=False)
 
-feature_columns = [tf.feature_column.numeric_column("image", shape=[784])]
+def make_input_fn(features, labels=None, training=True, batch_size=256):
+    def input_fn():
+        if labels is None:
+            dataset = tf.data.Dataset.from_tensor_slices(dict(features))
+        else:
+            dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+        if training:
+            dataset = dataset.shuffle(2000).repeat()
+        return dataset.batch(batch_size)
 
+    return input_fn
+
+
+feature_columns = [tf.feature_column.numeric_column('x', shape=[784])]
 estimator = tf.estimator.DNNClassifier(
     feature_columns=feature_columns,
     hidden_units=[500, 400, 400],
     n_classes=10,
-    optimizer=tf.train.AdamOptimizer(learning_rate=0.001),
+    optimizer=keras.optimizers.Adam(0.001),
     model_dir='model/estimator')
 
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={'image': mnist.train.images},
-    y=mnist.train.labels.astype(np.int32),
-    num_epochs=None,
-    batch_size=128,
-    shuffle=True)
+estimator.train(
+    input_fn=make_input_fn({'x': x_train}, y_train, training=True, batch_size=128),
+    steps=10000)
 
-estimator.train(input_fn=train_input_fn, steps=10000)
-
-test_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={'image': mnist.test.images},
-    y=mnist.test.labels.astype(np.int32),
-    num_epochs=1,
-    batch_size=128,
-    shuffle=False)
-
-accuracy_score = estimator.evaluate(input_fn=test_input_fn)['accuracy']
-print('\nTest accuracy: %g %%' % (accuracy_score * 100))
+# test_input_fn = tf.estimator.inputs.numpy_input_fn(
+#     x={'image': mnist.test.images},
+#     y=mnist.test.labels.astype(np.int32),
+#     num_epochs=1,
+#     batch_size=128,
+#     shuffle=False)
+#
+# accuracy_score = estimator.evaluate(input_fn=test_input_fn)['accuracy']
+# print('\nTest accuracy: %g %%' % (accuracy_score * 100))
